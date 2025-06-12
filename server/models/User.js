02d54a4855f4c015178d6f9e6411b9d277f86577
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-06-12 03:48:47 +0100

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -5,4 +5,4 @@ const userSchema = new mongoose.Schema({
     avatar: { type: String },
     createdAt: { type: Date, default: Date.now }
 });
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema); 
