2730b267a07220b572bb7c653366deb78750d41c
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-06-12 03:25:24 +0100

    Update quizResult.js

--- server/models/quizResult.js
+++ server/models/quizResult.js
@@ -1,1 +1,12 @@
-
+// server/models/QuizResult.js
+const mongoose = require('mongoose');
+const quizResultSchema = new mongoose.Schema({
+    nickname: { type: String, required: true },
+    subject: { type: String },
+    question: { type: Object },
+    userAnswer: { type: String },
+    correct: { type: Boolean },
+    timeSpent: { type: Number },
+    createdAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('QuizResult', quizResultSchema);
