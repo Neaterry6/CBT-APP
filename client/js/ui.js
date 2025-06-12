06122c89a24ff68af66885ab0d2471a974bc0dd3
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-06-12 03:09:55 +0100

    Update ui.js

--- client/js/ui.js
+++ client/js/ui.js
@@ -1,1 +1,7 @@
+export function showElement(id) {
+    document.getElementById(id).classList.remove('hidden');
+}
 
+export function hideElement(id) {
+    document.getElementById(id).classList.add('hidden');
+}
