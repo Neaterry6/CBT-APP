068172e5a001b8a09cc0d868071f085c3205a224
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-06-12 03:25:06 +0100

    Update studiedTopic.js

--- server/models/studiedTopic.js
+++ server/models/studiedTopic.js
@@ -1,1 +1,9 @@
-
+// server/models/StudiedTopic.js
+const mongoose = require('mongoose');
+const studiedTopicSchema = new mongoose.Schema({
+    nickname: { type: String, required: true },
+    subject: { type: String, required: true },
+    topic: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('StudiedTopic', studiedTopicSchema);
