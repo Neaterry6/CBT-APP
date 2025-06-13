18ecfe6c00203c89e6d59c16b9451e1afecd7da9
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-06-13 15:47:01 +0100

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,7 @@
-// server/models/User.js
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     nickname: { type: String, required: true },
     avatar: { type: String },
     createdAt: { type: Date, default: Date.now }
 });
-module.exports = mongoose.model('User', userSchema); 
+module.exports = mongoose.model('User', userSchema);
